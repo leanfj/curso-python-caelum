cbcb732c87076454d1a1220d831daa9026c6614d
leanfj <leanfj@hotmail.com>
2019-04-02 06:59:00 -0300

    Utilizaçao de funçao import

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -1,44 +1,45 @@
-print('*************************')
-print('*  Jogo da Adivinhação  *')
-print('*************************')
+def jogar():
+    print('*************************')
+    print('*  Jogo da Adivinhação  *')
+    print('*************************')
 
-numero_secreto = 42
-# total_tentativas = 4
-rodada = 1
-total_pontos = 1000
+    numero_secreto = 42
+    # total_tentativas = 4
+    rodada = 1
+    total_pontos = 1000
 
-print('Você tem {}'.format(total_pontos))
-nivel = int(input('Escolha o nivel de dificuldade de 1 a 3\n'))
+    print('Você tem {}'.format(total_pontos))
+    nivel = int(input('Escolha o nivel de dificuldade de 1 a 3\n'))
 
-if (nivel == 1):
-    total_tentativas = 20
-elif (nivel == 2):
-    total_tentativas = 10
-else:
-    total_tentativas = 5
+    if (nivel == 1):
+        total_tentativas = 20
+    elif (nivel == 2):
+        total_tentativas = 10
+    else:
+        total_tentativas = 5
 
-for rodada in range(1, total_tentativas + 1):
-    print('Tentativa {} de {}'.format(rodada, total_tentativas))
-    chute = int(input('Informe seu chute\n'))
-    print('Você digitou: ', chute)
+    for rodada in range(1, total_tentativas + 1):
+        print('Tentativa {} de {}'.format(rodada, total_tentativas))
+        chute = int(input('Informe seu chute\n'))
+        print('Você digitou: ', chute)
 
-    igual = chute == numero_secreto
-    maior = chute > numero_secreto
-    menor = chute < numero_secreto
-    if (igual):
-        print('Você acertou e fez {} pontos'.format(total_pontos))
-        break
-    elif (maior):
-        print('Seu chute foi maior e perdeu {} de {}'.format(
-            chute, total_pontos))
-        total_pontos = abs(total_pontos - chute)
-        print('E esta com {}'.format(total_pontos))
-    elif (menor):
-        print('Seu chute foi menor e perdeu {} de {}'.format(
-            chute, total_pontos))
-        total_pontos = abs(total_pontos - chute)
-        print('E esta com {}'.format(total_pontos))
+        igual = chute == numero_secreto
+        maior = chute > numero_secreto
+        menor = chute < numero_secreto
+        if (igual):
+            print('Você acertou e fez {} pontos'.format(total_pontos))
+            break
+        elif (maior):
+            print('Seu chute foi maior e perdeu {} de {}'.format(
+                chute, total_pontos))
+            total_pontos = abs(total_pontos - chute)
+            print('E esta com {}'.format(total_pontos))
+        elif (menor):
+            print('Seu chute foi menor e perdeu {} de {}'.format(
+                chute, total_pontos))
+            total_pontos = abs(total_pontos - chute)
+            print('E esta com {}'.format(total_pontos))
 
-    rodada = rodada + 1
+        rodada = rodada + 1
 
-print('Fim de Jogo')
+    print('Fim de Jogo')
--- jogos/forca.py
+++ jogos/forca.py
@@ -1,31 +1,32 @@
-print('**********************')
-print('*    Jogo da Forca   *')
-print('**********************')
+def jogar():
+    print('**********************')
+    print('*    Jogo da Forca   *')
+    print('**********************')
 
-palavra_secreta = 'mandioca'
-letras_acertadas = ['_', '_', '_', '_', '_', '_', '_', '_']
-enforcou = False
-acertou = False
-erro = 0
+    palavra_secreta = 'mandioca'
+    letras_acertadas = ['_', '_', '_', '_', '_', '_', '_', '_']
+    enforcou = False
+    acertou = False
+    erro = 0
 
-print(letras_acertadas)
-
-while (not enforcou and not acertou):
-    chute = input('Qual Letra\n')
-    if(chute in palavra_secreta):
-        posicao = 0
-        for letra in palavra_secreta:
-            if (chute.upper() == letra.upper()):
-                letras_acertadas[posicao] = letra
-            posicao += 1
-    else:
-        erro += 1
-    enforcou = erro == 6
-    acertou = '_' not in letras_acertadas
     print(letras_acertadas)
-if (acertou):
-    print('Você ganhou')
-else:
-    print('Você perdeu')
 
-print('Fim de Jogo')
+    while (not enforcou and not acertou):
+        chute = input('Qual Letra\n')
+        if(chute in palavra_secreta):
+            posicao = 0
+            for letra in palavra_secreta:
+                if (chute.upper() == letra.upper()):
+                    letras_acertadas[posicao] = letra
+                posicao += 1
+        else:
+            erro += 1
+        enforcou = erro == 6
+        acertou = '_' not in letras_acertadas
+        print(letras_acertadas)
+    if (acertou):
+        print('Você ganhou')
+    else:
+        print('Você perdeu')
+
+    print('Fim de Jogo')
