4bc5e7d8fd52d9fa9802270ace586b0cfb201825
leanfj <leanfj@hotmail.com>
2019-04-28 23:44:59 -0300

    Deixamos o jogo da forca com escolha de palavra randomica

--- jogos/forca.py
+++ jogos/forca.py
@@ -1,10 +1,25 @@
 def jogar():
+    import random
+    # Abrindo arquivo no modo leitura
+    # arquivo = open('palavras.txt', 'r')
+    # Array para receber palavras
+    palavras = []
+    # Abrindo arquivo e Incluindo cada palavra do arquivo no array
+    with open('palavras.txt', 'r') as arquivo:
+        for linha in arquivo:
+            linha = linha.strip()
+            palavras.append(linha)
+    # Fechando o uso do arquivo
+    # arquivo.close()
+    # Gerando numero randomico com range do tamanho do array criado
+    numero_randomico = random.randrange(0, len(palavras))
+
     print('**********************')
     print('*    Jogo da Forca   *')
     print('**********************')
 
-    palavra_secreta = 'mandioca'
-    letras_acertadas = ['_', '_', '_', '_', '_', '_', '_', '_']
+    palavra_secreta = palavras[numero_randomico].upper()
+    letras_acertadas = ['_' for letra in palavra_secreta]
     enforcou = False
     acertou = False
     erro = 0
@@ -13,6 +28,7 @@ def jogar():
 
     while (not enforcou and not acertou):
         chute = input('Qual Letra\n')
+        chute = chute.strip().upper()
         if(chute in palavra_secreta):
             posicao = 0
             for letra in palavra_secreta:
